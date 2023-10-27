0724469d173d6096649274242e925e17cc1dfd98
Gaurav Arora <62250726+SharkyG7@users.noreply.github.com>
2023-10-27 23:27:19 +0530

    Update python.py

--- codes/python.py
+++ codes/python.py
@@ -1,6 +1,5 @@
 '''
-        get coordinates using api
-        store coordinates
+        get coordinates using api  store coordinates
 EASY match with canteen coordinaes
         send notification
 EASY else run after 5 mins
