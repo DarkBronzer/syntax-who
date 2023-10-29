7979c96a42641c70484140b59c647e7de434c1c5
codingchamp27 <145395591+codingchamp27@users.noreply.github.com>
2023-10-29 12:10:22 +0530

    Delete codes/shops.ts

--- codes/shops.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from "mongoose";
-import {model} from "mongoose";
-
-const shopSchema = new mongoose.Schema({
-    location: {type: Object, required: true},
-    title: {type: String, required: true},
-});
-
-export default model('shops', shopSchema);
